1ce5b9435b287bc1392fa5af712276f6229f3377
jackfiled <xcrenchangjun@outlook.com>
2023-04-07 13:45:27 +0800

    Update at: 20230407134527

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -73,6 +73,7 @@ module.exports = {
                    'calculator',
                    'memory',
                    'instruction',
+                   'cpu',
                ],
                '/': [
                    '',
